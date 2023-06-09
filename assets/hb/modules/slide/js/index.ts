8a5829d20f11faf6970a7bd65fd22837c8902328
razonyang <razonyang@gmail.com>
2023-06-09 22:35:07 +0800

    feat: add touch move event

--- assets/hb/modules/slide/js/index.ts
+++ assets/hb/modules/slide/js/index.ts
@@ -1,30 +1,42 @@
 (() => {
-    'use strict'
+  'use strict'
 
-    const scroll = (element) => {
-        const inner = element.parentElement.querySelector('.slide-inner')
-        const step = inner.offsetWidth
-        let left = 0
-        if (element.classList.contains('slide-control-left')) {
-           left = inner.scrollLeft - step
-        } else {
-           left = inner.scrollLeft + step
-        }
-        inner.scroll({
-            left: left,
-            behavior: 'smooth'
-        })
+  const scroll = (element: HTMLElement, dir: string): void => {
+    const inner = element.parentElement?.querySelector('.slide-inner') as HTMLElement
+    const step = inner.offsetWidth
+    let left = 0
+    if (dir === 'left') {
+      left = inner.scrollLeft - step
+    } else {
+      left = inner.scrollLeft + step
     }
-    document.addEventListener('DOMContentLoaded', () => {
-        document.querySelectorAll('.slide-control-left').forEach(element => {
-            element.addEventListener('click', () => {
-                scroll(element)
-            })
-        })
-        document.querySelectorAll('.slide-control-right').forEach(element => {
-            element.addEventListener('click', () => {
-                scroll(element)
-            })
-        })
+    inner.scroll({
+      left,
+      behavior: 'smooth'
     })
+  }
+
+  document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll<HTMLElement>('.slide-control-left').forEach(element => {
+      element.addEventListener('click', () => {
+        scroll(element, 'left')
+      })
+    })
+    document.querySelectorAll<HTMLElement>('.slide-control-right').forEach(element => {
+      element.addEventListener('click', () => {
+        scroll(element, 'right')
+      })
+    })
+
+    const els = document.querySelectorAll<HTMLElement>('.slide-inner')
+    els.forEach(el => {
+      let startX = 0
+      el.addEventListener('touchstart', (e: TouchEvent) => {
+        startX = e.touches[0].clientX
+      })
+      el.addEventListener('touchend', (e: TouchEvent) => {
+        scroll(el, e.changedTouches[0].clientX > startX ? 'left' : 'right')
+      })
+    })
+  })
 })()
